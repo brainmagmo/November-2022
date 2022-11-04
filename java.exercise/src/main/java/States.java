52d6fac711801c8f9d85680393b78fef00f08f68
Trevor John Kleinstuber <trevor.kleinstuber@gmail.com>
2022-11-04 15:33:17 -0700

    fix typo "New York"

--- java.exercise/src/main/java/States.java
+++ java.exercise/src/main/java/States.java
@@ -4,6 +4,6 @@ public class States {
 	public static State Texas = new State("Texas", "TX");
 	public static State Louisiana = new State("Louisiana", "LA");
 	public static State California = new State("California", "CA");
-	public static State NewYork = new State("NewYork", "NY");
+	public static State NewYork = new State("New York", "NY");
 
 }
