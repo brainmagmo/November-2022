f333d2dbc777e4466f703156bd30e7dc9fc75d99
Trevor John Kleinstuber <trevor.kleinstuber@gmail.com>
2022-11-11 16:21:00 -0800

    fixed error on making array composed of same object multiple times causing side effect

--- data.access/src/test/java/data/access/Main.java
+++ data.access/src/test/java/data/access/Main.java
@@ -17,7 +17,7 @@ public class Main {
 	static final String SQL = 	""
 			+ "SELECT film_id "
 			+ "FROM sakila.film "
-			+ "WHERE title = 'Alien Center'"
+			+ "WHERE title = '"
 			;
 	//`film_in_stock`(IN p_film_id INT, IN p_store_id INT, OUT p_film_count INT)
 	static final String CALL = ""
@@ -27,18 +27,19 @@ public class Main {
 
 	public static DataRow[] ExecuteCall(
 			String call, 
-			int[] inputTypes, Object[] inputs,
-			int[] outputTypes, String[] outputNames
+			String[] paramNames, Object[] inputs, int[] inputTypes, 
+			int[] outputTypes, String[] outputNames 
 			) {
 
 		try {
 			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			CallableStatement stmt = conn.prepareCall(call);
-			for (int i = 1 ; i <= inputTypes.length ; i++) {
-				stmt.setObject(i, inputs[i-1]);
+			for (int i = 0 ; i < inputs.length ; i++) {
+				stmt.setObject(paramNames[i], inputs[i], inputTypes[i]);
+				//stmt.setObject(i, inputs[i-1]);
 			}
 			for (int i = 1 ; i <= outputTypes.length ; i++) {
-				stmt.registerOutParameter(i+inputTypes.length, outputTypes[i-1]);
+				stmt.registerOutParameter(i+inputs.length, outputTypes[i-1]);
 			}
 			boolean b = stmt.execute();
 			System.out.println("b: " + b);
@@ -46,14 +47,14 @@ public class Main {
 				ResultSet rs = stmt.getResultSet();
 				int numberOfCols = rs.getMetaData().getColumnCount();
 				System.out.println("cols: " + numberOfCols);
-				String[] currentRowData = new String[numberOfCols];
 				ArrayList<DataRow> outputList = new ArrayList<DataRow>();
 				while(rs.next()) {// -> false??????????
 					System.out.println("next");
-					for(int i = 1; i <= numberOfCols; i++) {
-						var s = rs.getString(i);
+					String[] currentRowData = new String[numberOfCols];
+					for(int i = 0; i < numberOfCols; i++) {
+						var s = rs.getString(i+1);
 						System.out.println(i + " " + s);
-						currentRowData[i-1] = rs.getString(i);
+						currentRowData[i] = s;
 					}
 					outputList.add(new StringRow(currentRowData));
 				}
@@ -66,22 +67,16 @@ public class Main {
 	}
 
 	public static String[] getInventoryIds(String film, int storeID) {
-		/*
-		 *(
-			String call, 
-			int[] inputTypes, Object[] inputs,
-			String[] outputNames, int[] outputTypes
-			)
-		 */
+
 		DatabaseAccessor db = new DatabaseUtility();
 		int filmID = Integer.valueOf(
-				db.ExecuteSingleCell(SQL));
-
+				db.ExecuteSingleCell(SQL + film + "'"));
+		String[] params = {"p_film_id", "p_store_id"};
 		Object[] inputs = {filmID, storeID};
 		int[] inTypes = {Types.INTEGER, Types.INTEGER};
 		String[] outNames = {"p_film_count"};
 		int[] outTypes = {Types.INTEGER};
-		DataRow[] data = ExecuteCall(CALL, inTypes, inputs, outTypes, outNames);
+		DataRow[] data = ExecuteCall(CALL, params, inputs, inTypes, outTypes, outNames);
 		String[] inventoryIDs = new String[data.length];
 		for(int i = 0 ; i < data.length ; i++) {
 			inventoryIDs[i] = data[i].getData()[0];
@@ -90,37 +85,39 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		DatabaseAccessor db = new DatabaseUtility();
-		int filmID = Integer.valueOf(
-				db.ExecuteSingleCell(SQL));
-
-		try {
-			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			CallableStatement stmt = conn.prepareCall("{CALL sakila.film_in_stock(? , ? , ?)}");
-
-			stmt.setInt(1, filmID);
-			stmt.setInt(2, STORE);
-
-			stmt.registerOutParameter(3, Types.INTEGER);
-
-			boolean b = stmt.execute();
-			System.out.println("b: " + b);
-			
-			if(b) {
-				ResultSet rs = stmt.getResultSet();
-				int numberOfCols = rs.getMetaData().getColumnCount();
-				System.out.println("cols: " + numberOfCols);
-				
-				while(rs.next()) {// -> false??????????
-					System.out.println("next");
-					for(int i = 1; i <= numberOfCols; i++) {
-						var s = rs.getString(i);
-						System.out.println(i + " " + s);
-					}
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} 
+//		DatabaseAccessor db = new DatabaseUtility();
+//		int filmID = Integer.valueOf(
+//				db.ExecuteSingleCell(SQL));
+		String[] results = getInventoryIds("Alien Center", 2);
+		for (var r : results) { System.out.println(r); }
+//		try {
+//			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+//			CallableStatement stmt = conn.prepareCall("{CALL sakila.film_in_stock(? , ? , ?)}");
+//
+//			stmt.setInt(1, filmID);
+//			stmt.setInt(2, STORE);
+//
+//			stmt.registerOutParameter(3, Types.INTEGER);
+//
+//			boolean b = stmt.execute();
+//			System.out.println("b: " + b);
+//			
+//			if(b) {
+//				ResultSet rs = stmt.getResultSet();
+//				int numberOfCols = rs.getMetaData().getColumnCount();
+//				System.out.println("cols: " + numberOfCols);
+//				
+//				while(rs.next()) {// -> false??????????
+//					System.out.println("next");
+//					for(int i = 1; i <= numberOfCols; i++) {
+//						var s = rs.getString(i);
+//						System.out.println(i + " " + s);
+//					}
+//				}
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} 
+		
 	}
 }
