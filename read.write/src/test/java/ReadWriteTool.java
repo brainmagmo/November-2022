4f6290ec933cf3da3a736acc74a913820e3f4389
Trevor John Kleinstuber <trevor.kleinstuber@gmail.com>
2022-12-08 19:33:41 -0800

    change implementation
    We will use built-in buffers and streams for optimization

--- read.write/src/test/java/ReadWriteTool.java
+++ read.write/src/test/java/ReadWriteTool.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -5,47 +6,19 @@ import java.util.HashMap;
 
 public class ReadWriteTool {
 
+
 	public static HashMap<String, String> getModelMakeMapFromFile(String filePath) {
-		FileReader reader;
-		var quickMap = new HashMap<String,String>();
-		var make = "";
-		var model = "";
-		var track = 0;
-		
+		var map = new HashMap<String,String>();
 		try {
-			reader = new FileReader(filePath);
-			var data = reader.read();	
-
-			while(data != -1) {
-				char c = (char) data;
-
-				if(c == '\n') {
-					quickMap.put(model.trim(), make.trim());
-					make = "";
-					model = "";	  		
-					track = 0;
-					data = reader.read();
-					continue;
-				}
-				if(c == ',') {
-					track += 1;
-					data = reader.read();
-					continue;
-				}
-
-				if(track == 0) {
-					make += c;
-				}	  		
-				if(track == 1) {
-					model += c;
-				}
-
-				data = reader.read();
-			}
-
-			quickMap.remove("Model");	  
-			reader.close();
-			return quickMap;			
+			var in = new BufferedReader(new FileReader(filePath));
+			
+			in.lines().forEach((line) -> {
+				String[] lineItems = line.split(",");
+				map.put(lineItems[1].trim(), lineItems[0].trim());
+			});;
+			map.remove("Model");	  
+			in.close();
+			return map;			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -54,12 +27,12 @@ public class ReadWriteTool {
 
 	public static ArrayList<Bass> getBassListFromFile(String fILEPATH) {
 		var bassList = new ArrayList<Bass>();
-			
+
 		getModelMakeMapFromFile(fILEPATH)
 		.forEach((model, make) -> {
 			bassList.add(new Bass(model, make));
 		});
-		
+
 		return bassList;
 	}
 
